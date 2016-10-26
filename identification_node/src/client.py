f4937a70d37b3934c069ddd303dabb02af3eb324
Matthias <matbloch@student.ethz.ch>
2016-10-26 16:54:25 +0200

    add numpy/opencv to image

--- identification_node/src/client.py
+++ identification_node/src/client.py
@@ -2,5 +2,5 @@ import socket
 
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # docker container is listening to port 80
-clientsocket.connect(('localhost', 80))
+clientsocket.connect(('127.0.0.1', 80))
 clientsocket.send('Hi there! I\'m your Client :)')
--- identification_node/src/core.py
+++ identification_node/src/core.py
@@ -1,36 +1,12 @@
-'''A sample of Python TCP server'''
-
 import socket
 
-# HOST = '127.0.0.1'     # Local host
-HOST = 'localhost'
-PORT = 80              # expose when starting the container!
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.bind(('localhost', 80))
+serversocket.listen(5) # become a server socket, maximum 5 connections
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-
-# maximum number of connectsion
-s.listen(1)
-
-print 'Waiting for connection...'
-conn, addr = s.accept()
-
-print 'Connected by client', addr
 while True:
-
-    # Wait for a connection
-    connect, address = s.accept()
-
-    # Typically fork at this point
-
-    # Receive up to 1024 bytes
-    resp = (connect.recv(1024)).strip()
-    # And if the user has sent a "SHUTDOWN"
-    # instruction, do so (ouch! just a demo)
-    if resp == "SHUTDOWN": break
-
-    # Send an answer
-    connect.send("You said '" + resp + "' to me\n")
-
-conn.close()
-print 'Server closed.'
+    connection, address = serversocket.accept()
+    buf = connection.recv(64)
+    if len(buf) > 0:
+        print buf
+        break
