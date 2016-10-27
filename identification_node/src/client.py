b1657855f5f69275286ec47445947843cdf0a180
Matthias <matbloch@student.ethz.ch>
2016-10-27 14:21:54 +0200

    Updating TCP server/client

--- identification_node/src/client.py
+++ identification_node/src/client.py
@@ -1,6 +1,29 @@
-import socket
+import socket  # for sockets
+import sys  # for exit
 
-clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    # create an AF_INET, STREAM socket (TCP)
+    clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error, msg:
+    print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
+    sys.exit();
+
+print 'Socket Created'
+
 # docker container is listening to port 80
-clientsocket.connect(('127.0.0.1', 80))
-clientsocket.send('Hi there! I\'m your Client :)')
+clientsocket.connect(('localhost', 80))
+
+try:
+    # Set the whole string
+    clientsocket.send('Hi there! I\'m your Client :)')
+except socket.error:
+    # Send failed
+    print 'Send failed'
+    sys.exit()
+
+print 'Message send successfully'
+
+# Now receive data
+reply = clientsocket.recv(4096)
+
+print reply
--- identification_node/src/core.py
+++ identification_node/src/core.py
@@ -1,12 +1,35 @@
 import socket
+import sys
 
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversocket.bind(('localhost', 80))
-serversocket.listen(5) # become a server socket, maximum 5 connections
+HOST = ''  # Symbolic name meaning all available interfaces
+PORT = 80  # Arbitrary non-privileged port
 
-while True:
-    connection, address = serversocket.accept()
-    buf = connection.recv(64)
-    if len(buf) > 0:
-        print buf
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print 'Socket created'
+
+try:
+    s.bind((HOST, PORT))
+except socket.error, msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+
+print 'Socket bind complete'
+
+s.listen(10)
+print 'Socket now listening'
+
+# now keep talking with the client
+while 1:
+    # wait to accept a connection - blocking call
+    conn, addr = s.accept()
+    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+
+    data = conn.recv(1024)
+    reply = 'OK...' + data
+    if not data:
         break
+
+    conn.sendall(reply)
+
+conn.close()
+s.close()
