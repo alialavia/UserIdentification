d80bbbc57db7df3b26d1d71fe643b72f6ef593d9
Matthias <matbloch@student.ethz.ch>
2016-11-03 16:12:11 +0100

    Add joint flags

--- include/base/UserIdentification.h
+++ include/base/UserIdentification.h
@@ -16,6 +16,37 @@
 
 #define NR_USERS 6
 
+namespace base
+{
+	enum JointType {
+		JointType_SpineBase = 1 << 0,
+		JointType_SpineMid = 1 << 1,
+		JointType_Neck = 1 << 2,
+		JointType_Head = 1 << 3,
+		JointType_ShoulderLeft = 1 << 4,
+		JointType_ElbowLeft = 1 << 5,
+		JointType_WristLeft = 1 << 6,
+		JointType_HandLeft = 1 << 7,
+		JointType_ShoulderRight = 1 << 8,
+		JointType_ElbowRight = 1 << 9,
+		JointType_WristRight = 1 << 10,
+		JointType_HandRight = 1 << 11,
+		JointType_HipLeft = 1 << 12,
+		JointType_KneeLeft = 1 << 13,
+		JointType_AnkleLeft = 1 << 14,
+		JointType_FootLeft = 1 << 15,
+		JointType_HipRight = 1 << 16,
+		JointType_KneeRight = 1 << 17,
+		JointType_AnkleRight = 1 << 18,
+		JointType_FootRight = 1 << 19,
+		JointType_SpineShoulder = 1 << 20,
+		JointType_HandTipLeft = 1 << 21,
+		JointType_ThumbLeft = 1 << 22,
+		JointType_HandTipRight = 1 << 23,
+		JointType_ThumbRight = 1 << 24,
+		JointType_Count = 25
+	};
+}
 
 // ---------------------------------------------------------------
 // Reference: http://stackoverflow.com/questions/81870/is-it-possible-to-print-a-variables-type-in-standard-c
