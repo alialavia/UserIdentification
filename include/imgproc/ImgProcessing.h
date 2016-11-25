09fc60b6e23887825e42eefa0184475c487a038c
Matthias Bloch <matbloch@ethz.ch>
2016-11-25 16:58:38 +0100

    Start image processing library

---
+++ include/imgproc/ImgProcessing.h
@@ -0,0 +1,9 @@
+#ifndef IMGPROC_IMGPROCESSING_H_
+#define IMGPROC_IMGPROCESSING_H_
+
+namespace imgproc {
+
+
+} // namespace
+
+#endif
---
+++ src/imgproc/ImgProcessing.cpp
@@ -0,0 +1,6 @@
+#include <math/Math.h>
+#include <opencv2/core/mat.hpp>
+#include <opencv2\imgproc.hpp>
+
+using namespace imgproc;
+
