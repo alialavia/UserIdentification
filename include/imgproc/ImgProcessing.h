34b38095db5706faddb4deddb0daebfd567683f1
Matthias Bloch <matbloch@ethz.ch>
2016-11-28 16:41:26 +0100

    adding focus measures

--- include/imgproc/ImgProcessing.h
+++ include/imgproc/ImgProcessing.h
@@ -1,8 +1,27 @@
 #ifndef IMGPROC_IMGPROCESSING_H_
 #define IMGPROC_IMGPROCESSING_H_
 
+#include <opencv2/core/mat.hpp>
+#include <opencv2\imgproc.hpp>
+
+namespace cv {
+	class Mat;
+}
+
 namespace imgproc {
 
+	class FocusMeasure
+	{
+	public:
+		// 'LAPV' - Variance of Laplacian algorithm (Pech2000)
+		static double LAPV(const cv::Mat& src);
+
+		// 'TENG' algorithm (Krotkov86)
+		static double TENG(const cv::Mat& src, int ksize);
+
+		// 'GLVN' - Graylevel local variance algorithm (Santos97)
+		static double GLVN(const cv::Mat& src);
+	};
 
 } // namespace
 
--- src/imgproc/ImgProcessing.cpp
+++ src/imgproc/ImgProcessing.cpp
@@ -1,6 +1,32 @@
 #include <math/Math.h>
-#include <opencv2/core/mat.hpp>
-#include <opencv2\imgproc.hpp>
+
+#include <imgproc\ImgProcessing.h>
 
 using namespace imgproc;
 
+double FocusMeasure::LAPV(const cv::Mat& src)
+{
+	cv::Mat lap;
+	cv::Laplacian(src, lap, CV_64F);
+	cv::Scalar mu, sigma;
+	cv::meanStdDev(lap, mu, sigma);
+	double focusMeasure = sigma.val[0] * sigma.val[0];
+	return focusMeasure;
+}
+
+double FocusMeasure::TENG(const cv::Mat& src, int ksize)
+{
+	cv::Mat Gx, Gy;
+	cv::Sobel(src, Gx, CV_64F, 1, 0, ksize);
+	cv::Sobel(src, Gy, CV_64F, 0, 1, ksize);
+	cv::Mat FM = Gx.mul(Gx) + Gy.mul(Gy);
+	double focusMeasure = cv::mean(FM).val[0];
+	return focusMeasure;
+}
+
+double FocusMeasure::GLVN(const cv::Mat& src) {
+	cv::Scalar mu, sigma;
+	cv::meanStdDev(src, mu, sigma);
+	double focusMeasure = (sigma.val[0] * sigma.val[0]) / mu.val[0];
+	return focusMeasure;
+}
