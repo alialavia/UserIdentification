9b86b1daff2c081a299019b185ce571a5c85be04
Matthias <matbloch@student.ethz.ch>
2016-12-01 14:37:21 +0100

    server config

---
+++ identification_node/src/config.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+"""Global server configuration - Request/Response names match models in src/request src/response"""
+CONFIG = {
+    "ROUTING": {
+        "REQUEST": {
+            "ID": {
+                "Identification": 1,                # identify user from image
+                "CollectEmbeddingsByID": 2,      # collect embeddings from images for user with ID XY
+                "CollectEmbeddingsByName": 3,    # collect embeddings from images for user with name XY - names assumed to be unique
+                "GetEmbedding": 4,                 # calculate embedding from image
+                "TrainClassifier": 5               # trigger classifier training
+            },
+            "NAME": {
+                1: "Identification",
+                2: "CollectEmbeddingsByID",
+                3: "CollectEmbeddingsByName",
+                4: "GetEmbedding",
+                5: "TrainClassifier"
+            }
+        },
+        "RESPONSE": {
+            "ID": {
+                "error": 999,
+            },
+            "NAME": {
+                999: "error",
+            }
+        }
+    },
+}
