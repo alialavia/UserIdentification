b51d9e839c74eaf429f628f384cc9ccf0b69e77f
Matthias <matbloch@student.ethz.ch>
2016-12-01 17:16:53 +0100

    fix typo

--- identification_node/src/response/Identification.py
+++ identification_node/src/response/Identification.py
@@ -6,10 +6,9 @@ class Identification:
 
     def __init__(self, server, conn, user_id, user_name):
 
-        resp_id = CONFIG['ROUTING']['RESPONSE']['ID']['Identification']
-
-        # send back response type
-        server.send_int(conn, 1)
+        # send back reponse identifier
+        resp_id = CONFIG['ROUTING']['RESPONSE']['ID'][self.__class__.__name__]
+        server.send_int(conn, int(resp_id))
 
         # send back user id
         server.send_int(conn, int(user_id))
