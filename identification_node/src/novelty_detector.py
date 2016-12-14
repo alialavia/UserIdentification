a3650d9c90bb956fe8b965b29a7a822c5a4c79cc
Matthias <matbloch@student.ethz.ch>
2016-12-14 10:22:25 +0100

    update

--- identification_node/src/novelty_detector.py
+++ identification_node/src/novelty_detector.py
@@ -48,28 +48,39 @@ class DataManager:
     # -- add data reduction method (clean out data set, time windows)
 
 
-
 class OneClassDetectorTree:
+    """
+    Possible Modes:
+    - Classify only using target class classifier (does not block training)
+    - Classify only on currently not used classes
+    - Classify using all classes (current)
+    """
 
     __classifiers = {}
     __classifier_states = {}
-    __status = 1
+    __retraining_counter = {}   # number of times classifier has been trained
     __training_data = {}
+    __status = 1
     __nr_classes = 0
-    __retraining_counter = {}
-    __max_model_outliers = 1
-    __verbose = True
+    __max_model_outliers = 1    # after x number of outlier features (per class), classifiers are retrained
+    __verbose = False
 
     STATUS_CLEAN = {
         0: 'shutdown',
         1: 'running'
     }
 
-    # ----------- training
+    # ---- class prediction threshold
+    # TODO: tune these parameters according to comparison with LFW - maybe adaptive threshold
+    __class_thresh = 0.8      # known person above thresh
+    __confusion_thresh = 0.01   # 1% confusion chance
+    __novelty_thresh = 0.01     # 1% novelty misdetection
 
+    # ---- multi-threaded training
     __tasks = Queue(maxsize=0)
     __num_threads = 3
     __training_lock = Lock()
+
 
     def __init__(self):
         # perform classifier training in tasks
@@ -83,12 +94,51 @@ class OneClassDetectorTree:
         self.__nr_classes += 1
         self.__classifier_states[class_id] = 0
         # collect the samples
-        self.collect_samples(class_id, class_samples)
+        self.__collect_samples(class_id, class_samples)
         # train the classifier
 
         return self.__retrain(class_id)
 
     def predict_class(self, samples):
+        """
+        Prediction casses:
+        - Only target class is identified with ratio X (high): Class
+        - Target and other class is identified with ration X (high) and Y (small): Class with small confusion
+        - Multiple classes are identified with small ratios Ys: Novelty
+        - No classes identified: Novelty
+        :param samples:
+        :return: Class ID, None (Novelty), -1 invalid dataset (multiple detections)
+        """
+        proba, class_ids = self.predict_proba(samples)
+
+        mask_0 = proba > 0
+
+        # no classes detected at all - novelty
+        if len(proba[mask_0]) == 0:
+            return None
+
+        mask_class = proba > self.__class_thresh
+        nr_classes = len(proba[mask_class])
+
+        if nr_classes > 0:
+            # class detected
+            if nr_classes > 1:
+                # multiple classes detected
+                return -1
+
+            # count if any element, except for class is above confusion ratio
+            if len(proba[(self.__confusion_thresh < proba) & (proba < self.__class_thresh)]) > 0:
+                return -1
+
+            return class_ids[mask_class]
+
+        else:
+            if len(proba[proba > self.__novelty_thresh]) > 0:
+                return -1
+
+            return None
+
+    def predict_proba(self, samples):
         predictions = self.__predict(samples)
         # analyze
         class_ids = []
@@ -96,27 +146,28 @@ class OneClassDetectorTree:
         for class_id, p in predictions.iteritems():
             probabilities.append(len(p[p > 0])/float(len(samples)))
             class_ids.append(class_id)
-        return probabilities, class_ids
+        return np.array(probabilities), np.array(class_ids)
 
     def process_labeled_stream_data(self, class_id, samples):
 
-        self.collect_samples(class_id, samples)
+        self.__collect_samples(class_id, samples)
         # check if incoming data explains the current model
         predictions = self.__predict(samples)
         self.__retraining_counter[class_id] += self.__contradictive_predictions(predictions, class_id)
 
         # trigger retraining
         if self.__retraining_counter[class_id] >= self.__max_model_outliers:
-            self.__retrain(class_id)
+            # threaded training
+            self.__add_training_task(class_id)
 
-    def collect_samples(self, class_id, samples):
+    # ------- Utilities
+
+    def __collect_samples(self, class_id, samples):
         # store data
         if class_id not in self.__training_data:
             self.__training_data[class_id] = samples
         else:
             self.__training_data[class_id] = np.concatenate((self.__training_data[class_id], samples))
-
-    # ------- Utilities
 
     def __predict(self, samples):
         """predict classes: for each sample on every class, tells whether or not (+1 or -1) it belongs to class"""
@@ -153,7 +204,7 @@ class OneClassDetectorTree:
             self.__classifiers[class_id].fit(self.__training_data[class_id])
             self.__retraining_counter[class_id] = 0
             self.__classifier_states[class_id] += 1
-        print "fitting took {} seconds".format(time.time() - start)
+        # print "fitting took {} seconds".format(time.time() - start)
         return True
 
     def __generate_classifier(self):
@@ -164,20 +215,24 @@ class OneClassDetectorTree:
 
     def __add_training_task(self, classifier_id):
         self.__tasks.put(classifier_id)
-        self.__tasks.task_done()
 
     def __classifier_trainer(self):
         if self.__verbose is True:
             print "--- starting classifier training thread"
         while True:
-            training_id = self.__tasks.get(True)
+
+            if self.__verbose is True:
+                print "--- thread training classifier"
+
+            # print "==== queue size: "+str(self.__tasks.qsize())
+            training_id = self.__tasks.get()
             self.__retrain(training_id)
             self.__tasks.task_done()
 
     def __deploy_classifier_trainers(self):
         for i in range(self.__num_threads):
             t = Thread(target=self.__classifier_trainer)
-            t.daemon = True # terminate if main thread ends
+            t.daemon = True  # terminate if main thread ends
             t.start()
 
     # -------- Not implemented yet
@@ -203,6 +258,7 @@ if __name__ == '__main__':
     np.random.shuffle(emb1)
     np.random.shuffle(emb2)
     np.random.shuffle(emb3)
+    np.random.shuffle(emb_lfw)
 
     split_set = np.array_split(emb1, 6)
     training_1 = split_set[0:3]
@@ -212,6 +268,8 @@ if __name__ == '__main__':
     training_2 = split_set[0:3]
     test_2 = split_set[3:6]
 
+    split_lfw = np.array_split(emb_lfw, 6)
+
     for i in range(3):
         if i==0:
             # add test class
@@ -220,9 +278,23 @@ if __name__ == '__main__':
             if not clf.init_classifier(2, training_2[i]):
                 print "--- initialization failed"
         else:
+
+
+            print "----PREDICTION: SET 1----------"
+            print clf.predict_class(test_1[i])
+            print "-------------------------------"
+
+            clf.process_labeled_stream_data(1, training_1[i])
+            clf.process_labeled_stream_data(2, training_2[i])
+
             print "----PREDICTION: SET 1----------"
             print clf.predict_class(test_1[i])
             print "-------------------------------"
             print "----PREDICTION: SET 2----------"
             print clf.predict_class(test_2[i])
             print "-------------------------------"
+
+
+
+    while True:
+        pass
