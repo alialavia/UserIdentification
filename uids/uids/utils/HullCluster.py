8d85dffda4bca05a07f66d038161fc5dfa182d1e
Matthias Bloch <matbloch@ethz.ch>
2017-02-13 14:54:05 +0100

    update

--- uids/uids/utils/HullCluster.py
+++ uids/uids/utils/HullCluster.py
@@ -9,7 +9,10 @@ from uids.utils.KNFilter import KNFilter
 
 
 class HullCluster:
-
+    """
+    Notes:
+        - Convex Hull is a subgraph of the Delauny Triangulation
+    """
     # ========= parameters
     # dimension reduction
     max_size = 100
@@ -21,7 +24,7 @@ class HullCluster:
     __verbose = False
 
     # ========= internal representation
-    data = []
+    __data = []
 
     # ========= logging
     __log = True
@@ -35,28 +38,31 @@ class HullCluster:
     def __init__(self):
         pass
 
+    def get_data(self):
+        return self.__data
+
     def update(self, samples):
 
         # init - add all samples if no data yet
-        if len(self.data) == 0:
-            self.data = samples
+        if len(self.__data) == 0:
+            self.__data = samples
 
         # =======================================
         # 1.  Reduce data/sample data
 
         if self.dim_reduction > 0:
-            basis, mean, var = ExtractMaxVarComponents(self.data, self.dim_reduction)
+            basis, mean, var = ExtractMaxVarComponents(self.__data, self.dim_reduction)
             self.log_expl_var.append(var)
         else:
-            basis, mean = ExtractSubspace(self.data, 0.8)
+            basis, mean = ExtractSubspace(self.__data, 0.8)
 
-        cluster_reduced = ProjectOntoSubspace(self.data, mean, basis)
+        cluster_reduced = ProjectOntoSubspace(self.__data, mean, basis)
         samples_reduced = ProjectOntoSubspace(samples, mean, basis)
         dims = np.shape(cluster_reduced)
         # select minimum data to build convex hull
         # min_nr_elems = dims[1] + 4
         if self.__verbose:
-            print "Reducing dimension: {}->{}".format(np.shape(self.data)[1], dims[1])
+            print "Reducing dimension: {}->{}".format(np.shape(self.__data)[1], dims[1])
 
         # =======================================
         # 2.  Calculate Convex Hull in subspace
@@ -64,7 +70,7 @@ class HullCluster:
         data_hull = cluster_reduced     # take all samples of data
         hull = Delaunay(data_hull)
         if self.__verbose:
-            print "Calculating data hull using {}/{} points".format(len(data_hull), len(self.data))
+            print "Calculating data hull using {}/{} points".format(len(data_hull), len(self.__data))
 
         # =======================================
         # 3.  Select new samples from outside convex hull
@@ -76,7 +82,7 @@ class HullCluster:
             print "Elements OUTSIDE hull (to include): {}/{}".format(nr_elems_outside_hull, len(samples))
 
         # add samples (samples need to be np.array)
-        self.data = np.concatenate((self.data, samples[outside_mask]))
+        self.__data = np.concatenate((self.__data, samples[outside_mask]))
 
         # =======================================
         # 4.  Recalculate hull with newly added points
@@ -84,16 +90,16 @@ class HullCluster:
         # If memory exceeded: Perform unrefinement process -
         # discharge sampling directions with lowest variance contribution
         if self.dim_reduction > 0:
-            nr_comps = self.dim_reduction if len(self.data) <= self.max_size else self.dim_removal
-            if len(self.data) > 150:
+            nr_comps = self.dim_reduction if len(self.__data) <= self.max_size else self.dim_removal
+            if len(self.__data) > 150:
                 nr_comps = self.dim_removal - 1
-            basis, mean, var = ExtractMaxVarComponents(self.data, nr_comps)
+            basis, mean, var = ExtractMaxVarComponents(self.__data, nr_comps)
         else:
             # automatic dimension selection (based on containing certain variance)
-            basis, mean = ExtractSubspace(self.data, 0.75)
+            basis, mean = ExtractSubspace(self.__data, 0.75)
 
-        cluster_reduced = ProjectOntoSubspace(self.data, mean, basis)
-        print "Recuding dimension: {}->{}".format(np.shape(self.data)[1], np.shape(cluster_reduced)[1])
+        cluster_reduced = ProjectOntoSubspace(self.__data, mean, basis)
+        print "Recuding dimension: {}->{}".format(np.shape(self.__data)[1], np.shape(cluster_reduced)[1])
         hull = Delaunay(cluster_reduced)
 
         # =======================================
@@ -112,29 +118,27 @@ class HullCluster:
 
         if self.__log:
             self.log_intra_deleted.append(len(cl_to_delete))
-            self.log_cl_size_orig.append(len(self.data))
-
-        if self.__verbose:
-            log.severe("4. Cleaning up samples from inside hull")
+            self.log_cl_size_orig.append(len(self.__data))
 
         print "Cleaning {} points from inside data".format(len(cl_to_delete))
 
         # Remove points from inside hull
-        self.data = np.delete(self.data, cl_to_delete, axis=0)
+        self.__data = np.delete(self.__data, cl_to_delete, axis=0)
 
         # =======================================
         # 6.  delete very similar points
 
-        filter = KNFilter(self.data, k=3, threshold=0.25)
-        max_removal = 10 if len(self.data) > 30 else 0
-        tmp = filter.filter_x_samples(max_removal)
-        print "--- Removing {} knn points".format(len(self.data)-len(tmp))
-        self.data = tmp
+        max_removal = 10 if len(self.__data) > 40 else 0
+        if max_removal > 0:
+            filter = KNFilter(self.__data, k=3, threshold=0.25)
+            tmp = filter.filter_x_samples(max_removal)
+            print "--- Removing {} knn points".format(len(self.__data)-len(tmp))
+            self.__data = tmp
 
         if self.__log:
-            self.log_cl_size_reduced.append(len(self.data))
+            self.log_cl_size_reduced.append(len(self.__data))
 
-        print "Cluster size: {}".format(len(self.data))
+        print "Cluster size: {}".format(len(self.__data))
 
     def plot_log(self):
         if not self.__log:
@@ -168,3 +172,34 @@ class HullCluster:
             plt.ylabel("Distance [euclidean]")
 
         plt.show()
+
+    # -------- deprecated
+
+    # -------------- first attempt: random deletion
+
+    # intracl_dist = pairwise_distances(self.data, self.data, metric='euclidean')
+    # min_dist = []
+    #
+    # for i_sample, distances in enumerate(intracl_dist):
+    #     # calc clostest point
+    #     min_dist.append(np.min(distances[distances > 0]))
+    #
+    # # sort by distance
+    # indices = range(0, len(self.data))
+    # min_dist, indices = zip(*sorted(zip(min_dist, indices)))
+    #
+    # if self.__log:
+    #     self.log_min_cl_sep.append(min_dist[0])
+    #
+    # if len(self.data) > self.max_size and self.__remove_near_pts:
+    #     # remove "worst" X points
+    #     nr_to_del = len(self.data) - self.max_size
+    #     step = 2
+    #     del_indices = np.unique(indices[0:(nr_to_del * step):step])
+    #
+    #     print "------------- REMOVING {}/{} points".format(nr_to_del, len(self.data))
+    #     # delete
+    #     self.data = np.delete(self.data, del_indices, axis=0)
+    #     self.log_dist_delete.append(len(del_indices))
+    # else:
+    #     self.log_dist_delete.append(0)
