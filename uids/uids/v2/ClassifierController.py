59876655c18507970728031719dc8e28e61db316
Matthias Bloch <matbloch@ethz.ch>
2017-04-18 15:58:15 +0200

    Update identification controller

--- uids/uids/v2/ClassifierController.py
+++ uids/uids/v2/ClassifierController.py
@@ -1,6 +1,8 @@
 import numpy as np
 from uids.utils.Logger import Logger as log
 from uids.data_models.StandardCluster import StandardCluster
+from sklearn.metrics.pairwise import *
+from uids.v2.set_metrics import *
 
 class BaseMetaController:
 
@@ -9,34 +11,60 @@ class BaseMetaController:
 
     # --------- HELPERS
 
-    def is_consistent_set(self, samples, relative_measure=False):
+    def is_consistent_set(self, samples):
         # check set for inconsistencies
         return True
 
     # ----------- unused
+    @staticmethod
+    def check_abod(samples):
+        """
+        Assumes that we have only few outliers
+        :param samples:
+        :return:
+        """
 
-    def __contradictive_predictions(self, predictions, target_class):
-        """used to trigger retraining"""
-        nr_cont_samples = 0
-        for class_id, col in predictions.iteritems():
-            nr_dects = len(col[col > 0])
-            # if wrongly predicted: class is -1 or w
-            nr_samples = len(col)
-            if class_id == target_class:
-                nr_cont_samples += (nr_samples - nr_dects)
-            else:
-                nr_cont_samples += nr_dects
-        return nr_cont_samples
+        abod = ABOD.get_set_score(samples)
+        nr_errors = np.count_nonzero(abod < 0.15)
+        # allowed errors
+        if nr_errors > 0:
+            return False
+        return True
+
+    # TODO: support weights - allow more deviation for low weighted samples
+    @staticmethod
+    def check_inter_sample_dist(samples, metric='euclidean'):
+
+        # calc pairwise distance
+        if metric == 'cosine':
+            dist = pairwise_distances(samples, samples, metric='cosine')
+            thresh = 0.7
+        elif metric == 'euclidean':
+            dist = pairwise_distances(samples, samples, metric='euclidean')
+            dist = np.square(dist)
+            thresh = 0.99
+        else:
+            raise ValueError
+
+        nr_errors = np.count_nonzero(dist > thresh)
+        # print "nr errors: {}, max: {}".format(nr_errors, np.max(dist))
+
+        # allowed errors
+        if nr_errors > 0:
+            return False
+        return True
 
 
 class IdentificationController(BaseMetaController):
     # raw CNN embeddings
     sample_queue = {}
+    sample_weight_queue = {}
+
     # link to classifier dict
     p_classifiers = None
 
-    __min_sample_length = 10
-    __identification_range = 5     # < __min_sample_length
+    __min_sample_length = 2    # at least 2 samples to build classifier
+    __save_sample_length = 5   # at least 5 samples to be safe
 
     def __init__(self, classifier_dict):
         BaseMetaController.__init__(self)
@@ -44,34 +72,66 @@ class IdentificationController(BaseMetaController):
 
     def drop_samples(self, tracking_id):
         self.sample_queue.pop(tracking_id, None)
+        self.sample_weight_queue.pop(tracking_id, None)
 
-    def try_to_identify(self, tracking_id, new_samples):
+    def accumulate_samples(self, tracking_id, new_samples, sample_weights=None, save_threshold=7):
+
+        # check for set inconsistency
+        samples_ok = BaseMetaController.check_inter_sample_dist(new_samples, metric='euclidean')
+
+        if not samples_ok:
+            log.severe("Identification set is inconsistent - disposing...")
+            # reset queue
+            self.sample_queue.pop(tracking_id, None)
+            self.sample_weight_queue.pop(tracking_id, None)
+            return False
+
+        # generate placeholder weights
+        if not sample_weights:
+            # 5 of 10
+            sample_weights = np.repeat(5, len(new_samples))
+
+        assert len(sample_weights) == len(new_samples)
+
         # add samples
         if tracking_id not in self.sample_queue:
             # initialize
             self.sample_queue[tracking_id] = new_samples
+            self.sample_weight_queue[tracking_id] = sample_weights
         else:
             # append
             self.sample_queue[tracking_id] = np.concatenate((self.sample_queue[tracking_id], new_samples))\
                                          if self.sample_queue[tracking_id].size \
                                          else new_samples
-        is_save_set = False
-        # TODO: return whole set or only last?
-        current_samples = self.sample_queue[tracking_id]
+            self.sample_weight_queue[tracking_id] = np.concatenate((self.sample_weight_queue[tracking_id], sample_weights))\
+                                         if self.sample_weight_queue[tracking_id].size \
+                                         else sample_weights
 
-        # try to do identification
+        is_save_set = False
+
+        # if set has save sample or is long enough
         if len(self.sample_queue[tracking_id]) >= self.__min_sample_length:
-            # meta recognition
-            if self.is_consistent_set(self.sample_queue[tracking_id], relative_measure=True):
-                # set is save - allow identification
-                is_save_set = True
-            else:
-                log.severe("Identification set is inconsistent - disposing...")
-            # dispose all samples
-            self.sample_queue.pop(tracking_id, None)
+            if len(self.sample_queue[tracking_id]) >= self.__save_sample_length\
+                    or np.count_nonzero(self.sample_weight_queue[tracking_id] >= save_threshold):
+
+                # check set consistency
+                samples_ok = BaseMetaController.check_inter_sample_dist(self.sample_queue[tracking_id], metric='euclidean')
+
+                if samples_ok:
+                    # set is save - allow identification
+                    is_save_set = True
+                else:
+                    # dispose all samples
+                    self.sample_queue.pop(tracking_id, None)
+                    self.sample_weight_queue.pop(tracking_id, None)
+                    log.severe("Identification set is inconsistent - disposing...")
+
+        # TODO: return whole set or only last?
+        current_samples = self.sample_queue.get(tracking_id, np.array([]))
+        current_weights = self.sample_weight_queue.get(tracking_id, np.array([]))
 
         # not enough save samples - return what we have so far
-        return is_save_set, current_samples
+        return is_save_set, current_samples, current_weights
 
 
 class UpdateController(BaseMetaController):
