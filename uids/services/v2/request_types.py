d6556152455b41c33ae1aeffbb7d27a0372a9ee3
Matthias Bloch <matbloch@ethz.ch>
2017-04-18 09:22:13 +0200

    V2 server base commit

---
+++ uids/services/v2/request_types.py
@@ -0,0 +1,222 @@
+#!/usr/bin/python
+import response_types as r
+from uids.utils.Logger import Logger as log
+# config
+from config import ROUTING
+r.ROUTING = ROUTING
+
+
+# --------------- IDENTIFICATION
+
+class ImageIdentificationPrealigned:
+
+    def __init__(self, server, conn, handle):
+
+        # receive tracking id
+        tracking_id = server.receive_uint(conn)
+
+        # receive images
+        images = server.receive_image_batch_squared_same_size(conn)
+
+        # generate embedding
+        embeddings = server.embedding_gen.get_embeddings(images, False)
+
+        if not embeddings.any():
+            r.Error(server, conn, "Could not generate face embeddings.")
+            return
+
+        is_save_set, current_samples = server.classifier.id_controller.try_to_identify(tracking_id, new_samples=embeddings)
+
+        # predict user id
+        id_pred, confidence = server.classifier.predict(current_samples)
+
+        # convert to integer
+        confidence = int(confidence*100.0)
+
+        # get user nice name
+        user_name = server.user_db.get_name_from_id(id_pred)
+
+        if user_name is None:
+            user_name = "unnamed"
+
+        if is_save_set:
+            profile_picture = None
+            # new identity
+            if id_pred < 0:
+                # unknown user
+                print "--- creating new user"
+                log.info('db', "Creating new User")
+                user_id = server.user_db.create_new_user("a_user")
+                server.user_db.print_users()
+                # add classifier
+                server.classifier.init_new_class(user_id, embeddings)
+            else:
+                # add data for training and return identification
+                server.update_controller.add_samples_for_inclusion(id_pred, current_samples)
+                # get profile picture
+                profile_picture = server.user_db.get_profile_picture(id_pred)
+
+            log.info('server', "User identification complete: {} [ID], {} [Username]".format(id_pred, user_name))
+            r.Identification(server, conn, int(id_pred), user_name, confidence=confidence, profile_picture=profile_picture)
+        else:
+            # return prediction and confidence - but no identification
+            r.IdentificationFeedback(server, conn, int(id_pred), user_name, confidence=confidence)
+
+
+# --------------- UPDATE
+
+
+
+class UpdatePrealignedRobust:
+
+    def __init__(self, server, conn, handle):
+        # receive user id
+        user_id = server.receive_uint(conn)
+
+        log.info('server', 'User Update (Aligned, Robust) for ID {}'.format(user_id))
+
+        # receive images
+        images = server.receive_image_batch_squared_same_size(conn)
+
+        # generate embedding
+        embeddings = server.embedding_gen.get_embeddings(images, False)
+
+        if not embeddings.any():
+            r.Error(server, conn, "Could not generate face embeddings.")
+            return
+
+        log.info('cl', "Starting to process stream data...")
+
+        # predict target user
+        predicted_id = 10
+
+        # accumulate samples - check for inconsistencies
+        is_safe, samples = server.classifier.update_controller.accumulate_save_samples(embeddings)
+
+        if is_safe:
+            # add to data model
+            server.classifier.data_controller.add_samples(user_id=user_id, new_samples=samples)
+            # add to classifier training queue
+            server.classifier.add_training_data(user_id, samples)
+        else:
+            # do prediction
+            pass
+
+        # return
+
+        # submit data
+        succ, conf = server.classifier.process_labeled_stream_data(user_id, embeddings, check_update=True)
+
+        if succ == None:
+            log.info('cls', "Update samples are unambiguously.")
+            r.Error(server, conn, "Update samples are unambiguously.")
+        elif succ == False:
+            # update was not classified as the labeled class
+            # force reidentification
+            r.Reidentification(server, conn)
+        else:
+            r.UpdateFeedback(server, conn, int(conf*100))
+
+
+# --------------- MISC
+
+
+class CancelIdentification:
+
+    def __init__(self, server, conn, handle):
+        # receive tracking id
+        tracking_id = server.receive_uint(conn)
+        # drop samples
+        server.classifier.id_controller.drop_samples(tracking_id=tracking_id)
+        r.OK(server, conn)
+
+
+class SaveDatabase:
+
+    def __init__(self, server, conn, handle):
+
+        # save user database
+        server.user_db.save()
+
+        r.OK(server, conn)
+
+
+class ImageAlignment:
+
+    def __init__(self, server, conn, handle):
+
+        log.info('server', "Image alignment")
+
+        # receive image
+        img = server.receive_rgb_image_squared(conn)
+
+        # align image
+        # innerEyesAndBottomLip, outerEyesAndNose
+        aligned = server.embedding_gen.align_face(img, 'outerEyesAndNose', 96)
+
+        if aligned is None:
+            r.Error(server, conn, "Could not align the image")
+            return
+
+        # send aligned image back
+        r.QuadraticImage(server, conn, aligned)
+
+
+class ProfilePictureUpdate:
+
+    def __init__(self, server, conn, handle):
+        # receive user id
+        user_id = server.receive_uint(conn)
+
+        log.info('server', 'Updating profile picture for user with ID {}'.format(user_id))
+
+        # receive images
+        image = server.receive_image_squared(conn)
+
+        # generate embedding
+        embedding = server.embedding_gen.get_embeddings([image])
+
+        if not embedding.any():
+            r.Error(server, conn, "Could not generate face embeddings.")
+            return
+
+        # predict user id
+        user_id_predicted = server.classifier.predict(embedding)
+
+        # check if correct user
+        if user_id_predicted is None:
+            r.Error(server, conn, "Label could not be predicted - Face is unambiguous.")
+            return
+        elif user_id_predicted != user_id:
+            # unknown user
+            r.Error(server, conn, "The profile image does not come from the same person!")
+            return
+
+        server.user_db.set_profile_picture(user_id, image)
+
+        # send back image
+        r.QuadraticImage(server, conn, image)
+
+
+class GetProfilePictures:
+
+    def __init__(self, server, conn, handle):
+
+        log.info('server', 'Getting all profile pictures')
+
+        uids, pictures = server.user_db.get_all_profile_pictures()
+
+        # send back image
+        r.ProfilePictures(server, conn, uids, pictures)
+
+
+class Ping:
+
+    def __init__(self, server, conn, handle):
+        r.Pong(server, conn)
+
+
+class Disconnect:
+    def __init__(self, server, conn, handle):
+        # break connection
+        handle[0] = False
