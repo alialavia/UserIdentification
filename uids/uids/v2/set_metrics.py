0143ca31146db72100b85bd887960f80c3c6ba7a
Matthias <matbloch@student.ethz.ch>
2017-04-26 14:08:09 +0200

    WABOD

--- uids/uids/v2/set_metrics.py
+++ uids/uids/v2/set_metrics.py
@@ -7,6 +7,7 @@ from numpy import (array, dot, arccos, clip)
 from numpy.linalg import norm
 from sklearn.metrics import pairwise_distances
 from sklearn.utils.extmath import fast_dot
+from uids.features.ConfidenceGen import WeightGenerator
 
 
 class SetMeanDistCosine:
@@ -98,7 +99,7 @@ class ABOD:
             raise Exception
 
         for i_sample, A in enumerate(test_samples):
-            varList = []
+            factor_list = []
             for i in range(len(reference_set)):
                 # select first point in reference set
                 B = reference_set[i]
@@ -115,7 +116,7 @@ class ABOD:
 
                     if np.array_equal(B, C):
                         log.error("Points are equal: B == C! Assuming classification of training point (ABOD 1000)")
-                        varList.append(1000)
+                        factor_list.append(1000)
                         print "Bi/Cj: {}/{}".format(i, j)
                         # sys.exit('ERROR\tangleBAC\tmath domain ERROR, |cos<AB, AC>| <= 1')
                         continue
@@ -131,8 +132,8 @@ class ABOD:
                         log.severe("ERROR\tABOF\tfloat division by zero! Trying to predict training point?'")
                         tmp = 500
                         # sys.exit('ERROR\tABOF\tfloat division by zero! Trying to predict training point?')
-                    varList.append(tmp)
-            factors.append(np.var(varList))
+                    factor_list.append(tmp)
+            factors.append(np.var(factor_list))
         return np.array(factors)
 
     @staticmethod
@@ -179,3 +180,100 @@ class ABOD:
         # faster than np.arccos
         angle = float(math.acos(cos_AB_AC_))    # <AB, AC> = arccos(cos<AB, AC>)
         return angle
+
+
+class WeightedABOD(ABOD):
+
+    weight_gen = None
+
+    def __init__(self):
+        self.weight_gen = WeightGenerator()
+
+    @staticmethod
+    def unbiased_weighted_var(values, weights):
+        average = np.average(values, weights=weights)
+        variance_biased = np.average((values - average) ** 2, weights=weights)  # Fast and numerically precise
+        V1 = np.sum(weights)
+        # V1_sqr = V1**2
+        V2 = np.sum(weights ** 2)
+        variance_unbiased = variance_biased / (1. - (V2 / (V1 ** 2)))
+        return variance_unbiased
+
+    @staticmethod
+    def biased_weighted_var(values, weights):
+        if True:
+            # no influence
+            pass
+            weights = weights/np.sum(weights)
+
+        average = np.average(values, weights=weights)
+        variance_biased = np.average((values - average) ** 2, weights=weights)  # Fast and numerically precise
+        return variance_biased
+
+    def get_weighted_score(self, test_samples, test_poses, ref_samples, ref_poses):
+
+        assert test_samples.ndim == 2
+        assert ref_samples.ndim == 2
+
+        dist_lookup = pairwise_distances(test_samples, ref_samples, metric='euclidean')
+
+        # print np.shape(dist_lookup[0])
+        factors = []
+        sample_weights = []
+
+        # if only one sample: cannot calculate abof
+        if len(ref_samples) < 3:
+            log.severe('Cannot calculate ABOF with {} reference samples (variance calculation needs at least 3 reference points)'.format(len(reference_set)))
+            raise Exception
+
+        for i_sample, A in enumerate(test_samples):
+            factor_list = []
+            weight_list = []
+            for i in range(len(ref_samples)):
+                # select first point in reference set
+                B = ref_samples[i]
+                # distance
+                AB = dist_lookup[i_sample][i]
+
+                for j in range(i + 1):
+                    if j == i:  # ensure B != C
+                        continue
+                    # select second point in reference set
+                    C = ref_samples[j]
+                    # distance
+                    AC = dist_lookup[i_sample][j]
+
+                    if np.array_equal(B, C):
+                        log.error("Points are equal: B == C! Assuming classification of training point (ABOD 1000)")
+                        factor_list.append(1000)
+                        print "Bi/Cj: {}/{}".format(i, j)
+                        # sys.exit('ERROR\tangleBAC\tmath domain ERROR, |cos<AB, AC>| <= 1')
+                        continue
+
+                    angle_BAC = ABOD.angleBAC(A, B, C, AB, AC)
+
+                    w1 = self.weight_gen.get_weight(test_poses[i_sample], ref_poses[i])
+                    w2 = self.weight_gen.get_weight(test_poses[i_sample], ref_poses[j])
+
+                    # get weights from i_sample to ref samples
+                    factor_weight = self.weight_gen.get_triplet_weight(test_poses[i_sample], ref_poses[i], ref_poses[j])
+                    weight_list.append(factor_weight)
+
+                    # compute each element of variance list
+                    try:
+                        # apply weighting
+                        tmp = angle_BAC / float(math.pow(AB *AC, 2))
+                    except ZeroDivisionError:
+                        log.severe("ERROR\tABOF\tfloat division by zero! Trying to predict training point?'")
+                        tmp = 500
+                        # sys.exit('ERROR\tABOF\tfloat division by zero! Trying to predict training point?')
+                    factor_list.append(tmp)
+
+                # print weight_list
+            # calculate weighted variance
+            biased_weighted_var = WeightedABOD.biased_weighted_var(np.array(factor_list), np.array(weight_list))
+            factors.append(biased_weighted_var)
+            sample_weights.append(np.average(weight_list))
+
+        return np.array(factors), np.array(sample_weights)
+
